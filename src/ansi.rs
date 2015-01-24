5c66853ceec4c13c618a63f3a2ffec524623a73a
Felipe Sere <felipesere@gmail.com>
2015-01-24 17:35:12 +0000

    It clears the screen

--- src/ansi.rs
+++ src/ansi.rs
@@ -7,7 +7,13 @@ struct Ansi<'a> {
 
 impl <'a> Ansi<'a> {
     pub fn escape(&mut self, message: &str) {
-        self.io.write("\x1b[something");
+        let mut esc = "\x1b[".to_string();
+        esc.push_str(message);
+        self.io.write(esc.as_slice());
+    }
+
+    pub fn clear(&mut self) {
+        self.escape("2J");
     }
 }
 
@@ -19,4 +25,12 @@ fn it_escapes_a_str() {
     ansi.escape("something");
     let inner_box = ansi.io;
     assert_eq!(inner_box.last(), "\x1b[something");
+}
+
+#[test]
+fn it_clears_the_screen() {
+    let mut ansi = Ansi { io: Box::new(FakeIO::new()) };
+    ansi.clear();
+    let inner_box = ansi.io;
+    assert_eq!(inner_box.last(), "\x1b[2J");
 }
