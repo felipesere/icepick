4dd9c7d0826a54710881ef87ced8c7f4231b9551
Carol Nichols <carol.nichols@gmail.com>
2015-01-28 21:31:06 -0500

    Remove redundant .trim().to_string()
    Configuration's clean method is taking care of this.

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,6 @@ fn extract_initial_query() -> Option<String> {
 
 fn read_lines() -> Vec<String> {
     std::io::stdio::stdin().lock().lines().map( |line| {
-        line.unwrap().trim().to_string()
+        line.unwrap()
     }).collect()
 }
