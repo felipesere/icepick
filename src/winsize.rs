cc05375e9e7ff73e7ef6337407cfe296a0a1eea8
Felipe Sere <felipesere@gmail.com>
2015-01-29 23:23:31 +0000

    Makes the constants platform dependant
    see: https://github.com/rust-lang/rust/blob/a6a6fadbb97268d2b3e47649d91053fbb4827266/src/libstd/sys/unix/tty.rs

--- src/winsize.rs
+++ src/winsize.rs
@@ -11,7 +11,13 @@ struct winsize {
     ws_ypixel: libc::c_ushort   /* vertical size, pixels */
 }
 
+#[cfg(any(target_os = "macos",
+          target_os = "freebsd"))]
 const TIOCGWINSZ: libc::c_ulong = 0x40087468;
+
+#[cfg(any(target_os = "linux",
+          target_os = "android"))]
+const TIOCGWINSZ: libc::c_ulong = 0x00005413;
 
 pub fn get_winsize(file: &File) -> IoResult<(usize, usize)> {
     let w = winsize { ws_row: 0, ws_col: 0, ws_xpixel: 0, ws_ypixel: 0 };
