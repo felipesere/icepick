e2ac4f90abe5d44f1683e24194aa31e3cde9b68e
Carol Nichols <carol.nichols@gmail.com>
2015-01-31 17:27:41 -0500

    Remove functionality for exiting with ESC
    Gary removed this functionality because arrow keys start with ESC so you
    have to wait to see if you get a code after ESC... so he decided just to
    take ESC out.

--- src/screen.rs
+++ src/screen.rs
@@ -32,7 +32,6 @@ impl <'a> Screen <'a>{
 
     pub fn handle_keystroke(&self, search: Search, input: &str) -> Search {
         match input {
-           "\u{1b}" => search.abort(),
            "\u{e}" => search.down(),
            "\u{10}" => search.up(),
            "\u{7f}" => search.backspace(),
@@ -67,14 +66,6 @@ impl <'a> Screen <'a>{
 
 
 #[cfg(test)]
-
-#[test]
-pub fn search_is_done_when_esc() {
-    let input = blank_search();
-    let screen = Screen::new();
-    let result = screen.handle_keystroke(input, "\u{1b}");
-    assert!(result.is_done());
-}
 
 #[test]
 pub fn moves_the_selection_down_for_ctrl_n() {
