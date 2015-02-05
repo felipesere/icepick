6bd810fbc205f7bf39ff558c37da26be87bd940f
Uku Taht <uku.taht@gmail.com>
2015-02-05 00:40:32 +0000

    Moves ScoreResult underneath SortedResultSet for clarity

--- src/sorted_result_set.rs
+++ src/sorted_result_set.rs
@@ -1,33 +1,6 @@
 use std::collections::BinaryHeap;
 use std::cmp::Ordering;
 
-pub struct ScoreResult<'a> {
-    pub quality: f32,
-    pub choice: &'a String,
-}
-
-impl<'a> Ord for ScoreResult<'a> {
-    fn cmp(&self, other: &ScoreResult) -> Ordering {
-        // Reverses ordering to make the binary max heap a min heap in Search::filter.
-        self.quality.partial_cmp(&other.quality).unwrap_or(Ordering::Equal).reverse()
-    }
-}
-
-impl<'a> PartialOrd for ScoreResult<'a> {
-    fn partial_cmp(&self, other: &ScoreResult) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<'a> Eq for ScoreResult<'a> {
-}
-
-impl<'a> PartialEq for ScoreResult<'a> {
-    fn eq(&self, other: &ScoreResult) -> bool {
-       self.quality == other.quality
-    }
-}
-
 pub struct SortedResultSet<'s> {
     results: BinaryHeap<ScoreResult<'s>>,
     size: usize,
@@ -52,5 +25,32 @@ impl<'s> SortedResultSet<'s> {
 
     pub fn sorted_vec(self) -> Vec<String> {
         self.results.into_sorted_vec().iter().map(|score_result| score_result.choice.clone()).collect()
+    }
+}
+
+pub struct ScoreResult<'a> {
+    pub quality: f32,
+    pub choice: &'a String,
+}
+
+impl<'a> Ord for ScoreResult<'a> {
+    fn cmp(&self, other: &ScoreResult) -> Ordering {
+        // Reverses ordering to make the binary max heap a min heap in Search::filter.
+        self.quality.partial_cmp(&other.quality).unwrap_or(Ordering::Equal).reverse()
+    }
+}
+
+impl<'a> PartialOrd for ScoreResult<'a> {
+    fn partial_cmp(&self, other: &ScoreResult) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Eq for ScoreResult<'a> {
+}
+
+impl<'a> PartialEq for ScoreResult<'a> {
+    fn eq(&self, other: &ScoreResult) -> bool {
+       self.quality == other.quality
     }
 }
