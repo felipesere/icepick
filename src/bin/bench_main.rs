1ffddd45ad3bf2c6dd61432e85fa3849db6f8a59
Uku Taht <uku.taht@gmail.com>
2015-02-09 16:40:34 +0000

    renames bench folder to benchmarks

--- src/bin/bench_main.rs
+++ src/bin/bench_main.rs
@@ -14,7 +14,7 @@ use std::old_io::{File, BufferedReader};
 #[allow(dead_code)]
 fn main() {
     let initial_query = extract_initial_query();
-    let lines = read_lines("bench/30000.txt".to_string());
+    let lines = read_lines("benchmarks/30000.txt".to_string());
 
     let config = Configuration::from_inputs(lines, initial_query, Some(20));
     let mut search = Search::blank(&config);
