0480491675f796785421ec74bab4617f9b4c5131
Uku Taht <uku.taht@gmail.com>
2015-02-14 01:33:57 +0000

    Converts ansi tests

---
+++ tests/ansi_test.rs
@@ -0,0 +1,45 @@
+#![feature(plugin)]
+#![plugin(stainless)]
+
+extern crate stainless;
+extern crate athena;
+
+mod test {
+    pub use athena::fake_tty::FakeIO;
+    pub use athena::tty::IO;
+    pub use athena::ansi::Ansi;
+
+    pub fn assert_results_in<F: FnMut(&mut Ansi)> (expected: &str, mut f: F) {
+        let mut ansi = Ansi { io: Box::new(FakeIO::new()) };
+
+        f(&mut ansi);
+        let inner_box = ansi.io;
+        assert_eq!(inner_box.last(), expected);
+    }
+
+    describe! ansi_test {
+        it "escapes_a_str" {
+            assert_results_in("\x1b[something", |ansi| { ansi.escape("something") });
+        }
+
+        it "clears_the_screen" {
+            assert_results_in("\x1b[2J", |ansi| { ansi.clear() });
+        }
+
+        it "hides_the_cursor" {
+            assert_results_in("\x1b[?251", |ansi| { ansi.hide_cursor() });
+        }
+
+        it "shows_the_cursor" {
+            assert_results_in("\x1b[?25h", |ansi| { ansi.show_cursor() });
+        }
+
+        it "sets_the_position" {
+            assert_results_in("\x1b[9;13H", |ansi| { ansi.set_position(8,12); });
+        }
+
+        it "prints_inverted" {
+            assert_results_in("\x1b[7mtest\x1b[0m", |ansi| { ansi.inverted("test"); });
+        }
+    }
+}
